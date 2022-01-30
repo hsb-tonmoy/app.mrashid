4bd6e6d75476abcec061685a1d40bc72f829b17b
hsb-tonmoy <hsb.tonmoy@gmail.com>
2022-01-29 19:54:53 -0500

    Started on dashboard.

---
+++ src/lib/svg/dashboard.js
@@ -0,0 +1,29 @@
+export const grid = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
+</svg>`;
+
+export const bell = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+</svg>`;
+
+export const message = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+</svg>`;
+
+export const help = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+<path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+</svg>`;
+
+export const chart = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z" />
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z" />
+</svg>`;
+
+export const user = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>`;
+
+export const settings = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+</svg>`;
---
+++ src/routes/dashboard/__layout.reset.svelte
@@ -0,0 +1,148 @@
+<script>
+	import { grid, bell, message, help, chart, user, settings } from '$lib/svg/dashboard';
+
+	let selected = true;
+</script>
+
+<main class="flex w-full bg-white h-screen">
+	<aside class="menu-border flex flex-col items-center w-[4vw] py-20">
+		<div id="logo" class="h-[10%]">
+			<img class="w-10 h-10" src="/logo.png" alt="app.mrashid.net" />
+		</div>
+		<div id="menu" class="flex flex-col h-[70%] gap-y-8">
+			<span class="menu-icon" class:menu-icon-active={selected}>{@html grid}</span>
+			<span class="menu-icon">{@html bell}</span>
+			<span class="menu-icon">{@html message}</span>
+			<span class="menu-icon">{@html help}</span>
+			<span class="menu-icon">{@html chart}</span>
+		</div>
+		<div id="profile" class="flex flex-col justify-end h-[20%] gap-y-4">
+			<span class="menu-icon">{@html user}</span>
+			<span class="menu-icon">{@html settings}</span>
+		</div>
+	</aside>
+	<aside class="flex w-[15vw] py-20 px-8">
+		<ol class="relative border-l border-gray-200 dark:border-gray-700">
+			<li class="mb-10 ml-6">
+				<h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+					Flowbite Application UI v2.0.0 <span
+						class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3"
+						>Latest</span
+					>
+				</h3>
+				<time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+					>Released on January 13th, 2022</time
+				>
+				<p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+					Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar,
+					and pre-order E-commerce & Marketing pages.
+				</p>
+				<a
+					href="#"
+					class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+					><svg
+						class="w-4 h-4 mr-2"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							fill-rule="evenodd"
+							d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
+							clip-rule="evenodd"
+						/></svg
+					> Download ZIP</a
+				>
+				<span
+					class="absolute flex items-center justify-center w-6 h-6 bg-blue-200 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"
+				>
+					<svg
+						class="w-3 h-3 text-blue-600 dark:text-blue-400"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							fill-rule="evenodd"
+							d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+							clip-rule="evenodd"
+						/></svg
+					>
+				</span>
+			</li>
+			<li class="mb-10 ml-6">
+				<span
+					class="absolute flex items-center justify-center w-6 h-6 bg-blue-200 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"
+				>
+					<svg
+						class="w-3 h-3 text-blue-600 dark:text-blue-400"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							fill-rule="evenodd"
+							d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+							clip-rule="evenodd"
+						/></svg
+					>
+				</span>
+				<h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+					Flowbite Figma v1.3.0
+				</h3>
+				<time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+					>Released on December 7th, 2021</time
+				>
+				<p class="text-base font-normal text-gray-500 dark:text-gray-400">
+					All of the pages and components are first designed in Figma and we keep a parity between
+					the two versions even as we update the project.
+				</p>
+			</li>
+			<li class="mb-10 ml-6">
+				<span
+					class="absolute flex items-center justify-center w-6 h-6 bg-blue-200 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"
+				>
+					<svg
+						class="w-3 h-3 text-blue-600 dark:text-blue-400"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							fill-rule="evenodd"
+							d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+							clip-rule="evenodd"
+						/></svg
+					>
+				</span>
+				<h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+					Flowbite Library v1.2.2
+				</h3>
+				<time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+					>Released on December 2nd, 2021</time
+				>
+				<p class="text-base font-normal text-gray-500 dark:text-gray-400">
+					Get started with dozens of web components and interactive elements built on top of
+					Tailwind CSS.
+				</p>
+			</li>
+		</ol>
+	</aside>
+	<div id="main" class="flex w-[56vw] bg-[#FCFBFE] py-20"><slot /></div>
+	<aside class="flex w-[25vw py-20]">notes</aside>
+</main>
+
+<style lang="postcss">
+	.menu-border {
+		border: 1px solid transparent;
+		border-image: linear-gradient(to bottom, rgb(229 231 235), rgb(249 250 251)) 1;
+	}
+
+	.menu-icon {
+		@apply text-[#B4BDCB] h-12 w-12 p-2 rounded-lg transition-all duration-300 ease-in-out;
+	}
+
+	.menu-icon:hover {
+		@apply bg-black text-white;
+	}
+
+	.menu-icon-active {
+		@apply bg-black text-white;
+	}
+</style>
