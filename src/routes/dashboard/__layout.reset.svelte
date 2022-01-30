56a6e340ebfdb464b0ace9e78da2fd6f7e66e2fd
hsb-tonmoy <hsb.tonmoy@gmail.com>
2022-01-30 17:34:02 -0500

    Added timeline.

--- src/routes/dashboard/__layout.reset.svelte
+++ src/routes/dashboard/__layout.reset.svelte
@@ -1,7 +1,28 @@
 <script>
-	import { grid, bell, message, help, chart, user, settings } from '$lib/svg/dashboard';
+	import {
+		grid,
+		bell,
+		message,
+		help,
+		chart,
+		user,
+		settings,
+		folder,
+		correct
+	} from '$lib/svg/dashboard';
 
 	let selected = true;
+
+	const timeline_options = [
+		{ title: 'Account Creation', icon: folder, active: true },
+		{ title: 'File Opening', icon: folder, active: false },
+		{ title: 'Document Submission', icon: folder, active: false },
+		{ title: 'College Application', icon: folder, active: false },
+		{ title: 'Reception of I-20', icon: folder, active: false },
+		{ title: 'SEVIS Payment', icon: folder, active: false },
+		{ title: 'DS-160 Submission', icon: folder, active: false },
+		{ title: 'Visa Fee Payment', icon: folder, active: false }
+	];
 </script>
 
 <main class="flex w-full bg-white h-screen">
@@ -21,108 +42,37 @@
 			<span class="menu-icon">{@html settings}</span>
 		</div>
 	</aside>
-	<aside class="flex w-[15vw] py-20 px-8">
-		<ol class="relative border-l border-gray-200 dark:border-gray-700">
-			<li class="mb-10 ml-6">
-				<h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-					Flowbite Application UI v2.0.0 <span
-						class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3"
-						>Latest</span
+	<aside class="flex items-center w-[15vw] py-20 px-8 border-r border-r-gray-100 right-shadow">
+		<div class="column w-[300px] relative  border-r-2 timeline-border">
+			<!-- Timeline Start -->
+			<div class="timeline_wrapper relative">
+				{#each timeline_options as item}
+					<div
+						class="timeline_item flex justify-end items-center relative pr-[48px] pb-[32px] pl-[30px]"
 					>
-				</h3>
-				<time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-					>Released on January 13th, 2022</time
-				>
-				<p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-					Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar,
-					and pre-order E-commerce & Marketing pages.
-				</p>
-				<a
-					href="#"
-					class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-					><svg
-						class="w-4 h-4 mr-2"
-						fill="currentColor"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
-						><path
-							fill-rule="evenodd"
-							d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
-							clip-rule="evenodd"
-						/></svg
-					> Download ZIP</a
-				>
-				<span
-					class="absolute flex items-center justify-center w-6 h-6 bg-blue-200 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"
-				>
-					<svg
-						class="w-3 h-3 text-blue-600 dark:text-blue-400"
-						fill="currentColor"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
-						><path
-							fill-rule="evenodd"
-							d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-							clip-rule="evenodd"
-						/></svg
-					>
-				</span>
-			</li>
-			<li class="mb-10 ml-6">
-				<span
-					class="absolute flex items-center justify-center w-6 h-6 bg-blue-200 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"
-				>
-					<svg
-						class="w-3 h-3 text-blue-600 dark:text-blue-400"
-						fill="currentColor"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
-						><path
-							fill-rule="evenodd"
-							d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-							clip-rule="evenodd"
-						/></svg
-					>
-				</span>
-				<h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-					Flowbite Figma v1.3.0
-				</h3>
-				<time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-					>Released on December 7th, 2021</time
-				>
-				<p class="text-base font-normal text-gray-500 dark:text-gray-400">
-					All of the pages and components are first designed in Figma and we keep a parity between
-					the two versions even as we update the project.
-				</p>
-			</li>
-			<li class="mb-10 ml-6">
-				<span
-					class="absolute flex items-center justify-center w-6 h-6 bg-blue-200 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"
-				>
-					<svg
-						class="w-3 h-3 text-blue-600 dark:text-blue-400"
-						fill="currentColor"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
-						><path
-							fill-rule="evenodd"
-							d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-							clip-rule="evenodd"
-						/></svg
-					>
-				</span>
-				<h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-					Flowbite Library v1.2.2
-				</h3>
-				<time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-					>Released on December 2nd, 2021</time
-				>
-				<p class="text-base font-normal text-gray-500 dark:text-gray-400">
-					Get started with dozens of web components and interactive elements built on top of
-					Tailwind CSS.
-				</p>
-			</li>
-		</ol>
+						<div class="title text-right">
+							<h2 class="font-medium">{item.title}</h2>
+						</div>
+						<div
+							class={`${
+								item.active ? 'timeline-icon-active' : 'timeline-icon'
+							} relative flex w-[52px] h-[52px] ml-[20px] justify-center items-center rounded-full z-10`}
+						>
+							<span class="w-7 h-7">{@html item.icon}</span>
+						</div>
+						<div
+							class="symbol absolute left-auto top-auto right-[-11px] bottom-auto w-5 h-5 border-4 bg-green-600 border-green-600 rounded-full z-auto"
+						>
+							<span class="w-4 h-4 text-white">{@html correct}</span>
+						</div>
+					</div>
+				{/each}
+				<div
+					class="step-line-venom absolute left-auto top-0 right-[73px] mx-auto h-full bottom-0 w-[3px] timeline-line -z-10"
+				/>
+				<!-- Timeline End -->
+			</div>
+		</div>
 	</aside>
 	<div id="main" class="flex w-[56vw] bg-[#FCFBFE] py-20"><slot /></div>
 	<aside class="flex w-[25vw py-20]">notes</aside>
@@ -138,11 +88,31 @@
 		@apply text-[#B4BDCB] h-12 w-12 p-2 rounded-lg transition-all duration-300 ease-in-out;
 	}
 
-	.menu-icon:hover {
+	.menu-icon:hover,
+	.menu-icon-active {
 		@apply bg-black text-white;
 	}
 
-	.menu-icon-active {
-		@apply bg-black text-white;
+	.right-shadow {
+		box-shadow: 0 0 15px rgba(229, 231, 235, 0.2);
+		clip-path: inset(0px -15px 0px 0px);
+	}
+
+	.timeline-line {
+		background-color: rgba(73, 74, 125, 0.32);
+		border-color: rgba(73, 74, 125, 0.32);
+	}
+
+	.timeline-border {
+		border-color: rgba(73, 74, 125, 0.32);
+	}
+
+	.timeline-icon {
+		@apply text-[#AEAFC5] bg-white border-2 border-[#AEAFC5];
+	}
+
+	.timeline-icon:hover,
+	.timeline-icon-active {
+		@apply text-white bg-[#2db27c];
 	}
 </style>
