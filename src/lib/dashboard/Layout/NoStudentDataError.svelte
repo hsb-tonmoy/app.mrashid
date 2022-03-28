914881f91deef131b69b68e99668e4baeaaae9fb
hsb-tonmoy <hsb.tonmoy@gmail.com>
2022-03-28 12:14:21 -0400

    Changed help email address.

--- src/lib/dashboard/Layout/NoStudentDataError.svelte
+++ src/lib/dashboard/Layout/NoStudentDataError.svelte
@@ -4,8 +4,8 @@
 	</h2>
 	<p class="text-sm lg:text-base text-gray-700">
 		On the contrary, if you've already submitted the form but still seeing this message, please send
-		an email to <a href="mailto:help@mrashid.net" class="text-blue-600 hover:underline"
-			>help@mrashid.net</a
+		an email to <a href="mailto:hello@mrashid.net" class="text-blue-600 hover:underline"
+			>hello@mrashid.net</a
 		> and include your full name and email address used in the form.
 	</p>
 </div>
