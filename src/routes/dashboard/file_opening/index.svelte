811ae995782b2601a3b508d8ea898bc35a5ef591
hsb-tonmoy <hsb.tonmoy@gmail.com>
2022-05-17 20:06:55 -0400

    Updated desc.

--- src/routes/dashboard/file_opening/index.svelte
+++ src/routes/dashboard/file_opening/index.svelte
@@ -9,6 +9,17 @@
 	<title>app.mrashid.net - File Opening</title>
 </svelte:head>
 
-<div class="page-body flex flex-col">
-	<h4 class="text-2xl text-lightText font-medium mb-8">File Opening</h4>
+<div class="page-body flex flex-col justify-center">
+	<p class="text-sm lg:text-2xl text-gray-700">
+		Please visit the regional office of Aemers in Bangladesh or call <a
+			href="tel:8801781373911"
+			class="">+88 0178 137 3911</a
+		>
+		to discuss how to initiate the file opening process. A fee is associated with this stage and our
+		representative will be happy to assist you.
+		<span class="italic font-bold"
+			>Please note that you can still enjoy the benefits and resources of this app without opening
+			the file.</span
+		>
+	</p>
 </div>
