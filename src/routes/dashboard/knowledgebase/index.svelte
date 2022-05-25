246d13f3e0b3fbea2d61c2bffd39c3453c835fd1
hsb.tonmoy <hsb.tonmoy@gmail.com>
2022-05-25 16:53:52 -0400

    Redirecting non-admin users to dash

--- src/routes/dashboard/knowledgebase/index.svelte
+++ src/routes/dashboard/knowledgebase/index.svelte
@@ -1,3 +1,15 @@
+<script context="module">
+	export async function load({ session }) {
+		if (session.user.account_type != 6) {
+			return {
+				status: 302,
+				redirect: '/dashboard'
+			};
+		}
+		return {};
+	}
+</script>
+
 <script>
 	import { submit_identifier, current_page_title } from '$lib/dashboard/stores';
 	import ArticleList from '$lib/dashboard/Knowledgebase/ArticleList.svelte';
